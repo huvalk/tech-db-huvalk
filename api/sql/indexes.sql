066656e625064b28ab0e4a347efe49bd0b2d93ba
Куклин С. И <akiraq@yandex.ru>
2020-04-18 05:07:30 +0300

    trieng to optimise indexes

--- api/sql/indexes.sql
+++ api/sql/indexes.sql
@@ -7,20 +7,19 @@ CREATE INDEX index_post_root ON post (root);
 CREATE UNIQUE INDEX index_post_thread_id ON post (thread_id, id);
 
 CREATE UNIQUE INDEX index_post_thread_id_parent_root ON post (thread_id, id, parent, root);
+-- drop index index_post_thread_id_parent_root;
+-- заменено index_post_parent_thread_id и index_post_id_parents_root
+
 
 CREATE UNIQUE INDEX index_post_id_root ON post (id, root);
 
--- CREATE UNIQUE INDEX index_user_nickname ON users (nickname, id);
+CREATE UNIQUE INDEX index_user_nickname ON users (nickname, id);
+-- drop index index_user_nickname;
 -- index_users_full
 
--- CREATE UNIQUE INDEX index_forum_slug_id ON forum (slug, id);
+CREATE UNIQUE INDEX index_forum_slug_id ON forum (slug, id);
+-- drop index index_forum_slug_id;
 -- index_forum_full
-
-CREATE INDEX index_thread_forum_id ON thread (forum_id);
-
-CREATE INDEX index_thread_forum_slug ON thread (forum_slug);
-
-CREATE INDEX index_thread_sort ON thread (forum_id, created);
 
 -- ////
 --
@@ -29,14 +28,27 @@ CREATE INDEX index_thread_sort ON thread (forum_id, created);
 -- // заменить коаклис на нул, посмотреть, что получиться
 -- CREATE UNIQUE INDEX index_thread_id_slug ON thread (id, COALESCE(thread.slug, ''));
 --
--- // можно добавить, неплохо используется
--- CREATE UNIQUE INDEX index_users_of_forum_forum_id_user_id ON users_of_forum (forum_id, user_id);
---
--- //
-
 -- // покрывающие
 
-CREATE UNIQUE INDEX index_users_of_forum_forum_id_user_id ON users_of_forum (forum_id, user_id);
-CREATE INDEX index_users_full ON users (nickname, id) include (email, about, fullname);
-CREATE INDEX index_forum_full on forum (slug, id) include (title, moderator_name, threads, posts);
-CREATE UNIQUE INDEX index_thread_full ON thread (forum_id, created) include (id, slug, title, message, forum_slug, author_name, created, votes);
+CREATE  INDEX index_users_of_forum_forum_id_user_id ON users_of_forum (forum_id, user_id);
+CREATE  INDEX index_users_full ON users (nickname, id) include (email, about, fullname);
+
+-- // не используетс
+-- CREATE  INDEX index_forum_full on forum (slug, id) include (posts, slug, threads, title, moderator_name);
+
+CREATE  INDEX index_thread_full ON thread (forum_id, created) include (id, slug, title, message, forum_slug, author_name, created, votes);
+
+-- // небольшая прибавка есть
+--
+-- drop index index_users_of_forum_forum_id_user_id
+-- drop index index_users_full
+-- -- drop index index_forum_full
+-- drop index index_thread_full
+--
+-- ///
+
+CREATE UNIQUE INDEX index_post_id_parents_root ON post (id) include (parents, root, thread_id);
+-- drop index index_post_id_parents_root
+
+CREATE INDEX index_post_parent_thread_id ON post (parent, thread_id) include (id);
+-- drop index index_post_parent_thread_id
